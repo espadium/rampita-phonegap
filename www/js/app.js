091860476f727b36baa4db21b11fb5512cb56184
Gabriel Osorio <gabriel.osorio@neonroots.com>
2014-07-22 22:53:19 -0300

    Add basic Ember.js app as template.

---
+++ www/js/app.js
@@ -0,0 +1,13 @@
+// TODO: This should be separated into its respective files.
+
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  // put your routes here
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
